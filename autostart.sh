59226e630d07f610a2af8bb12ef3637cded4df77
Kamil Dudkowski <kamil.dudkowski@gmail.com>
2018-06-11 04:59:36 +0200

    Skrypty

---
+++ autostart.sh
@@ -0,0 +1,9 @@
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install htop net-tools openssh-server proftpd espeak geany git python3-dev python3-venv python3-pip apache2 php mysql-server mysql-common mysql-client phpmyadmin mplayer
+sudo apt autoclean
+sudo apt autoremove
+sudo apt dist-upgrade
+sudo service apache restart
+sudo service mysql restart
+pip3 install --user mps-youtube youtube-dl
+
