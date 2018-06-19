4ebb61992f1d0e725d35b65a6b68ec558235eb1b
Kamil Dudkowski <kamil.dudkowski@gmail.com>
2018-06-19 02:16:24 +0200

    i2c, lcd, pogoda

--- test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import sys
-# the mock-0.3.1 dir contains testcase.py, testutils.py & mock.py
-sys.path.append('/usr/share/domobot')
-
-import dapi as config
-
-print config.dapi
